db7005eb358557afad84defe7567c66b078d8576
matejmosko <matej.mosko@gmail.com>
2019-11-20 16:53:52 +0100

    Refactor code for electron-forge

--- forge.config.js
+++ forge.config.js
@@ -1,8 +1,25 @@
+const path = require('path');
 module.exports = {
-  packagerConfig: {},
-  makers: [
+  electronPackagerConfig: {
+    icon: path.resolve(__dirname, 'src/assets/icons/icon.ico')
+  },
+  makers: [{
+      name: "@electron-forge/maker-flatpak",
+      config: {
+        options: {
+          categories: ["Games"]
+        }
+      }
+    },
     {
-      name: '@electron-forge/maker-deb'
+      name: "@electron-forge/maker-deb",
+      config: {}
+    },
+    {
+      name: "@electron-forge/maker-squirrel",
+      config: {
+        name: "game-of-worlds"
+      }
     }
   ]
 }
